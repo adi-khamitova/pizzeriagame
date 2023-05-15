33623eda363b7d2e62f56a778b9f97ce432d810a
Adelya Khamitova <adi.khamitova@gmail.com>
2023-05-15 23:58:55 +0300

    first version without any maths

---
+++ pizzeria_v1/pizzeria/Window1.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pizzeria
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Vector relativeMousePos;
+        FrameworkElement draggedObject;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        void StartDrag(object sender, MouseButtonEventArgs e)
+        {
+            draggedObject = (FrameworkElement)sender;
+            relativeMousePos = e.GetPosition(draggedObject) - new Point();
+            draggedObject.MouseMove += OnDragMove;
+            draggedObject.LostMouseCapture += OnLostCapture;
+            draggedObject.MouseUp += OnMouseUp;
+            Mouse.Capture(draggedObject);
+        }
+        void OnDragMove(object sender, MouseEventArgs e)
+        {
+            UpdatePosition(e);
+        }
+        void UpdatePosition(MouseEventArgs e)
+        {
+            var point = e.GetPosition(DragArena);
+            var newPos = point - relativeMousePos;
+            Canvas.SetLeft(draggedObject, newPos.X);
+            Canvas.SetTop(draggedObject, newPos.Y);
+        }
+        void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            FinishDrag(sender, e);
+            Mouse.Capture(null);
+        }
+
+        void OnLostCapture(object sender, MouseEventArgs e)
+        {
+            FinishDrag(sender, e);
+        }
+        void FinishDrag(object sender, MouseEventArgs e)
+        {
+            draggedObject.MouseMove -= OnDragMove;
+            draggedObject.LostMouseCapture -= OnLostCapture;
+            draggedObject.MouseUp -= OnMouseUp;
+            UpdatePosition(e);
+        }
+
+        private void cheesebutton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
